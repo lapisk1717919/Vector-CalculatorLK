78c71122cf59aa3013d359584a88339635381cb3
Krueger <1717919@mukilteo.wednet.edu>
2024-02-07 10:04:06 -0800

    nearly done

--- Vector Calculator/Program.cs
+++ Vector Calculator/Program.cs
@@ -6,11 +6,53 @@ namespace Vector_Calculator
     {
         static void Main(string[] args)
         {
-            Vector test1 = new Vector(1, 2, 3);
-            Vector test2 = new Vector(-1, 2, -3);
-            
-            // This should be 0, 4, 0
-            Console.WriteLine(Vector.Add(test1, test2));
+            //i love making colorful and descriptive interfaces
+            //vector input
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Input first vector. One value at a time!");
+            Console.ForegroundColor = ConsoleColor.White;
+            Vector test1 = new Vector(Convert.ToSingle(Console.ReadLine()), Convert.ToSingle(Console.ReadLine()), Convert.ToSingle(Console.ReadLine()));
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Input second vector. One value at a time!");
+            Console.ForegroundColor = ConsoleColor.White;
+            Vector test2 = new Vector(Convert.ToSingle(Console.ReadLine()), Convert.ToSingle(Console.ReadLine()), Convert.ToSingle(Console.ReadLine()));
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Input Scalar.");
+            Console.ForegroundColor = ConsoleColor.White;
+            float scalar = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            //tests
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Vector.Add: " + (test1 + test2) + " (v1 + v2)");
+            Console.WriteLine("Vector.Subtract: " + (test1 - test2) + " (v1 - v2)");
+            Console.WriteLine("");
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Vector.Negate: " + (-test1) + " (v1 * -1)");
+            Console.WriteLine("Vector.Scale: " + (test1 * scalar) + " (v1 * scalar)");
+            Console.WriteLine("");
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("GetMagnitude: " + test1.GetMagnitude);
+            Console.WriteLine("GetDirection: " + test1.GetDirection);
+            Console.WriteLine("");
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("DotProduct: " + Vector.DotProduct(test1, test2));
+            Console.WriteLine("CrossProduct: " + Vector.CrossProduct(test1, test2));
+            Console.WriteLine("AngleBetween (Rad): " + Vector.AngleBetween(test1, test2));
+            Console.WriteLine("AngleBetween (Deg): " + (Vector.AngleBetween(test1, test2) / MathF.PI) * 180); // thank you braden
+            Console.WriteLine("");
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Vector.Normalize: " + Vector.Normalize(test1) + " (Product [Should be ~ 1]: " + Vector.Normalize(test1).GetMagnitude + ")");
+            Console.WriteLine("Equal: " + (test1 == test2));
+            Console.WriteLine("");
+
+
+            Console.ForegroundColor = ConsoleColor.White;
         }
     }
 }
--- Vector Calculator/Vector.cs
+++ Vector Calculator/Vector.cs
@@ -2,7 +2,8 @@
 
 namespace Vector_Calculator
 {
-    public class Vector
+
+    public struct Vector
     {
         public static readonly Vector Zero = new Vector(1, 1, 1);
         public static readonly Vector One = new Vector(0, 0, 0);
@@ -23,70 +24,92 @@ namespace Vector_Calculator
             return $"<{x}, {y}, {z}>";
         }
 
-        public float GetMagnitude()
+        // squares each value, adds them together, and then square roots it
+        public float GetMagnitude => MathF.Sqrt((x * x) + (y * y) + (z * z));
+
+        // takes atan of y/x. this one is just readable
+        public float GetDirection => MathF.Atan(y / x);
+
+        public static Vector operator+(Vector v1, Vector v2)
         {
-            // ADD CODE HERE, THEN REMOVE BELOW LINE
-            throw new NotImplementedException();
+            // adds the values together and returns the result
+            return new Vector(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
         }
 
-        public float GetDirection()
+        public static Vector operator-(Vector v)
         {
-            // ADD CODE HERE, THEN REMOVE BELOW LINE
-            throw new NotImplementedException();
+            // * -1 to negate the values
+            return new Vector(v.x * -1, v.y * -1, v.z * -1);
         }
 
-        public static Vector Add(Vector v1, Vector v2)
+        public static Vector operator-(Vector v1, Vector v2)
         {
-            // ADD CODE HERE, THEN REMOVE BELOW LINE
-            throw new NotImplementedException();
+            // subtracts v2 from v1
+            return new Vector(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
         }
 
-        public static Vector Negate(Vector v)
+        public static Vector operator*(Vector v, float scalar)
         {
-            // ADD CODE HERE, THEN REMOVE BELOW LINE
-            throw new NotImplementedException();
-        }
-
-        public static Vector Subtract(Vector v1, Vector v2)
-        {
-            // ADD CODE HERE, THEN REMOVE BELOW LINE
-            throw new NotImplementedException();
-        }
-
-        public static Vector Scale(Vector v)
-        {
-            // ADD CODE HERE, THEN REMOVE BELOW LINE
-            throw new NotImplementedException();
+            // multiplies each value by the scalar
+            return new Vector(v.x * scalar, v.y * scalar, v.z * scalar);
         }
 
         public static Vector Normalize(Vector v)
         {
-            // ADD CODE HERE, THEN REMOVE BELOW LINE
-            throw new NotImplementedException();
+            // i still don't quite understand this but thank you perry
+            float p = 1 / v.GetMagnitude;
+            return new Vector(v.x * p, v.y * p, v.z * p);
         }
 
         public static float DotProduct(Vector v1, Vector v2)
         {
-            // ADD CODE HERE, THEN REMOVE BELOW LINE
-            throw new NotImplementedException();
+            // multiplies each value together and then adds the products
+            return (v1.x * v2.x) + (v1.y * v2.y) + (v1.z * v2.z);
         }
 
         public static Vector CrossProduct(Vector v1, Vector v2)
         {
-            // ADD CODE HERE, THEN REMOVE BELOW LINE
-            throw new NotImplementedException();
+            // uhh
+            float x = (v1.y * v2.z) - (v1.z * v2.y);
+            float y = (v1.z * v2.x) - (v1.x * v2.z);
+            float z = (v1.x * v2.y) - (v1.y * v2.x);
+            return new Vector(x, y, z);
         }
 
-        public static Vector AngleBetween(Vector v1, Vector v2)
+        public static float AngleBetween(Vector v1, Vector v2)
         {
-            // ADD CODE HERE, THEN REMOVE BELOW LINE
-            throw new NotImplementedException();
+            // theta = cos-1 of dotproduct over test1 magnitude * test2 magnitude
+            return MathF.Acos(Vector.DotProduct(v1, v2) / (v1.GetMagnitude * v2.GetMagnitude));
         }
 
         public static Vector ProjectOnto(Vector v1, Vector v2)
         {
             // ADD CODE HERE, THEN REMOVE BELOW LINE
             throw new NotImplementedException();
+        }
+
+        public static bool operator==(Vector v1, Vector v2)
+        {
+            // braden told me this is not optimal but i don't care
+            if (v1.x == v2.x && v1.y == v2.y && v1.z == v2.z)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool operator!=(Vector v1, Vector v2)
+        {
+            if (v1.x == v2.x && v1.y == v2.y && v1.z == v2.z)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
         }
     }
 }
